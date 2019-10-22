d14f988d2bfe82e8385157bdd9300b3434b58af3
Duc Bui <buixuanduc2930@gmail.com>
2019-10-21 22:50:22 -0400

    Testing using vectors!

--- bankingsystem.cpp
+++ bankingsystem.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -13,32 +14,40 @@ class Account {
     };
 };
 
+vector<Account> arr;
+
 void addRecord() {
 
-  // Declare variables
-  int account_number;
-  string first_name;
-  string last_name;
-  double balance;
+    // Declare variables
+    int account_number;
+    string first_name;
+    string last_name;
+    double balance;
 
-  // Add records
-  cout << "Enter account number: ";
-  cin >> account_number;
-  cout << "Enter first name: ";
-  cin >> first_name;
-  cout << "Enter last name: ";
-  cin >> last_name;
-  cout << "Enter balance: ";
-  cin >> balance;
+    // Add records
+    cout << "Enter account number: ";
+    cin >> account_number;
+    cout << "Enter first name: ";
+    cin >> first_name;
+    cout << "Enter last name: ";
+    cin >> last_name;
+    cout << "Enter balance: ";
+    cin >> balance;
 
-  Account new_account = Account(account_number=account_number, first_name=first_name, last_name=last_name, balance=balance);
-  cout << "Dear Mr/Mrs " << first_name << " " << last_name << endl;
-  cout << "You created a new account with an account number of " << account_number;
-  cout << ", with a balance of " << balance << endl;
+    Account new_account = Account(account_number=account_number, first_name=first_name, last_name=last_name, balance=balance);
+    cout << "Dear Mr/Mrs " << first_name << " " << last_name << endl;
+    cout << "You created a new account with an account number of " << account_number;
+    cout << ", with a balance of " << balance << endl;
+
+    arr.push_back(new_account);
+
+    cout << arr.size() << endl;
+
 };
 
 int main()
 {
-  addRecord();
-  return 0;
+    addRecord();
+    addRecord();
+    return 0;
 }
