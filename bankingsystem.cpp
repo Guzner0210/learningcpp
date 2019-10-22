126faece14b479e8a13ae7bfb28c08d7e612a9bb
Duc Bui <buixuanduc2930@gmail.com>
2019-10-21 22:50:22 -0400

    Add add_record function!

---
+++ bankingsystem.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using namespace std;
+
+class Account {
+  public:
+
+    Account(int account_number, string first_name, string last_name, double balance){
+        account_number = account_number;
+        first_name = first_name;
+        last_name = last_name;
+        balance = balance;
+    };
+};
+
+void addRecord() {
+
+  // Declare variables
+  int account_number;
+  string first_name;
+  string last_name;
+  double balance;
+
+  // Add records
+  cout << "Enter account number: ";
+  cin >> account_number;
+  cout << "Enter first name: ";
+  cin >> first_name;
+  cout << "Enter last name: ";
+  cin >> last_name;
+  cout << "Enter balance: ";
+  cin >> balance;
+
+  Account new_account = Account(account_number=account_number, first_name=first_name, last_name=last_name, balance=balance);
+  cout << "Dear Mr/Mrs " << first_name << " " << last_name << endl;
+  cout << "You created a new account with an account number of " << account_number;
+  cout << ", with a balance of " << balance << endl;
+};
+
+int main()
+{
+  addRecord();
+  return 0;
+}
