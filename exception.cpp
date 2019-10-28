15a02f0cad68828ed7af4c9a53434f69d4e1f3d6
Duc Bui <buixuanduc2930@gmail.com>
2019-10-28 13:33:23 -0400

    learning exception s

---
+++ exception.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+ using namespace std;
+
+void mightGoWrong() {
+
+  bool err = true;
+
+  if (err) {
+    throw 8;
+  }
+
+}
+
+ int main() {
+
+   try {
+    mightGoWrong();
+   }
+   catch(int e) {
+     cout << "Error code " << e << endl; 
+   }
+
+
+   return 0;
+ }
