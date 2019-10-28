d003efce5c18972ffe39d0bbc1f3ec38a161c4d4
Duc Bui <buixuanduc2930@gmail.com>
2019-10-28 13:42:55 -0400

    learning exceptions

--- exception.cpp
+++ exception.cpp
@@ -3,22 +3,39 @@
 
 void mightGoWrong() {
 
-  bool err = true;
+  bool error1 = false;
+  bool error2 = true;
 
-  if (err) {
-    throw 8;
+  if (error1) {
+    throw "Something went wrong";
+  }
+
+  if (error2) {
+    throw string("Something else went wrong.");
   }
 
 }
 
+ void useMightGoWrong() {
+   mightGoWrong();
+ }
+
  int main() {
 
    try {
-    mightGoWrong();
+    useMightGoWrong();
    }
    catch(int e) {
-     cout << "Error code " << e << endl; 
+     cout << "Error code " << e << endl;
    }
+   catch(char const * e) {
+     cout << "Error message: " << e << endl;
+   }
+   catch(string &e) {
+     cout << "string error message: " << e << endl;
+   }
+
+   cout << "Still running" << endl;
 
 
    return 0;
